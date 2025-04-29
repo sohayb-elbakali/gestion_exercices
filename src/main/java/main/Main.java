242442a133ac33dec6b59cd202fd0d0d82773649
sohayb.elbakali <96049863+sohayb-elbakali@users.noreply.github.com>
2025-04-29 22:44:12 +0100

    add

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -3,16 +3,34 @@ package main;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
+
+import java.io.InputStream;
 
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/login.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 400, 250);
+        Scene scene = new Scene(fxmlLoader.load(), 600, 700);
         scene.getStylesheets().add(getClass().getResource("/css/styles.css").toExternalForm());
-        primaryStage.setTitle("Login");
+        
+        // Set the application title
+        primaryStage.setTitle("Education Management System");
+        
+        // Set the application icon
+        try (InputStream iconStream = getClass().getResourceAsStream("/../../lib/images/education.png")) {
+            if (iconStream != null) {
+                primaryStage.getIcons().add(new Image(iconStream));
+            } else {
+                System.err.println("Could not load application icon");
+            }
+        } catch (Exception e) {
+            System.err.println("Error loading icon: " + e.getMessage());
+        }
+        
         primaryStage.setScene(scene);
+        primaryStage.setResizable(true);
         primaryStage.show();
     }
 
